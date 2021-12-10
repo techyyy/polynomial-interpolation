db0090c6d02458f1ce47b592b1d5bdbdd1edb10e
techyyy <ukrainianbtch@gmail.com>
2021-12-10 16:13:45 +0200

    lab 5 added

--- poly.py
+++ poly.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy.polynomial import polynomial as p
+from numpy.polynomial import Polynomial as P
 import matplotlib.pyplot as plt
 
 
@@ -18,6 +19,17 @@ def get_divided_diffs(y, h):
     return div_diffs
 
 
+def newtons_polynom(y, x):
+    div_diffs = get_divided_diffs(y, x[1] - x[0])
+    poly = [0]
+    for i in range(0, x.size):
+        poly_add = [div_diffs[0, i]]
+        for j in range(0, i):
+            poly_add = p.polymul(poly_add, [-x[j], 1])
+        poly = p.polyadd(poly, poly_add)
+    return poly
+
+
 def lagrange_polynom(y, x):
     poly = [0]
     for i in range(0, y.size):
@@ -29,15 +41,35 @@ def lagrange_polynom(y, x):
     return poly
 
 
-def newtons_polynom(y, x):
-    div_diffs = get_divided_diffs(y, x[1] - x[0])
-    poly = [0]
-    for i in range(0, x.size):
-        poly_add = [div_diffs[0, i]]
-        for j in range(0, i):
-            poly_add = p.polymul(poly_add, [-x[j], 1])
-        poly = p.polyadd(poly, poly_add)
-    return poly
+def get_spline(y, x):
+    tridiag_a = np.zeros((x.size - 2, x.size - 2))
+    b = np.empty(x.size - 2)
+
+    for i in range(0, x.size - 2):
+        b[i] = (y[i + 2] - y[i + 1]) / (x[i + 2] - x[i + 1]) - (y[i + 1] - y[i]) / (x[i + 1] - x[i])
+        if i > 0:
+            tridiag_a[i, i - 1] = (x[i + 1] - x[i]) / 6
+        tridiag_a[i, i] = (x[i + 2] - x[i]) / 3
+        if i < x.size - 3:
+            tridiag_a[i, i + 1] = (x[i + 2] - x[i + 1]) / 6
+
+    m = np.linalg.solve(tridiag_a, b)
+    m = np.insert(m, 0, 0)
+    m = np.insert(m, m.size, 0)
+
+    polys = []
+
+    for i in range(0, x.size - 1):
+        poly = P([0])
+        h = x[i + 1] - x[i]
+        poly += P([x[i + 1], -1]) ** 3 * (m[i] / (6 * h))
+        poly += P([-x[i], 1]) ** 3 * (m[i + 1] / (6 * h))
+        poly += P([x[i + 1], -1]) * ((y[i] - m[i] * h * h / 6) / h)
+        poly += P([-x[i], 1]) * ((y[i + 1] - m[i + 1] * h * h / 6) / h)
+
+        polys.append(poly.coef)
+
+    return polys
 
 
 n = 10
@@ -72,3 +104,17 @@ print("Newton's polynomial:", newton_poly)
 quadratic_graph(x_value, y_value)
 plt.plot(real, p.polyval(real, newton_poly))
 plt.show()
+
+print("Natural cubic splines:")
+
+spline_polys = get_spline(y_value, x_value)
+
+quadratic_graph(x_value, y_value)
+for i in range(0, n - 1):
+    x_lower = -4 + (8 / (n-1)) * i
+    x_upper = -4 + (8 / (n-1)) * (i+1)
+    x_interval = np.linspace(x_lower, x_upper, 50)
+    print("{0} for [{1}; {2}]".format(spline_polys[i], x_lower, x_upper))
+    plt.plot(x_interval, p.polyval(x_interval, spline_polys[i]))
+plt.show()
+
